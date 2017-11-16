e9eaf31cf4653cc09e6fb0c7bbf4f5566e77aeb9
Mark <grimelios@gmail.com>
2017-11-15 20:23:19 -0600

    Initial commit.

---
+++ KilobitCup/MainGame.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace KilobitCup
+{
+	/// <summary>
+	/// Main game class.
+	/// </summary>
+	public class MainGame : Game
+	{
+		private const int DefaultWidth = 400;
+		private const int DefaultHeight = 300;
+
+		private GraphicsDeviceManager graphics;
+		private SpriteBatch spriteBatch;
+
+		public MainGame()
+		{
+			graphics = new GraphicsDeviceManager(this)
+			{
+				PreferredBackBufferWidth = DefaultWidth,
+				PreferredBackBufferHeight = DefaultHeight
+			};
+
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
+
+		/// <summary>
+		/// Initializes the game.
+		/// </summary>
+		protected override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		/// <summary>
+		/// Loads game content.
+		/// </summary>
+		protected override void LoadContent()
+		{
+			spriteBatch = new SpriteBatch(GraphicsDevice);
+		}
+
+		/// <summary>
+		/// Updates the game.
+		/// </summary>
+		protected override void Update(GameTime gameTime)
+		{
+		}
+
+		/// <summary>
+		/// Draws the game.
+		/// </summary>
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
+		}
+	}
+}
---
+++ KilobitCup/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace KilobitCup
+{
+	/// <summary>
+	/// Main program for the bit cup.
+	/// </summary>
+    public static class Program
+    {
+		/// <summary>
+		/// Main function for the program.
+		/// </summary>
+        [STAThread]
+        public static void Main()
+        {
+	        using (MainGame game = new MainGame())
+	        {
+				game.Run();
+			}
+        }
+    }
+}
