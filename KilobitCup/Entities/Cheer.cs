85c0e672bc98f897dde494857961671d3d3184a0
Mark <grimelios@gmail.com>
2017-11-17 22:05:23 -0600

    Adding the Cheer entity for later.

---
+++ KilobitCup/Entities/Cheer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KilobitCup.Entities
+{
+	/// <summary>
+	/// Represents an animated cheermote with physics data.
+	/// </summary>
+	public class Cheer : Entity
+	{
+	}
+}
